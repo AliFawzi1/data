f06acc6cf61021e970e5ecc2e46e9d140260ed0c
AliFawzi1 <131494245+AliFawzi1@users.noreply.github.com>
2023-04-30 02:11:05 +0300

    Add files via upload

---
+++ data.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h1>Data Science Tools and Ecosystem</h1>
+
+# This notebook provides a summary of the tools and ecosystem used in Data Science.
+
+# There are several popular programming languages utilized by Data Scientists, including:
+# 
+# Python: Python is widely used in data science due to its simplicity, readability, and large number of libraries and frameworks specifically designed for data analysis and machine learning, such as NumPy, Pandas, and Scikit-learn.
+# 
+# R: R is a language and environment for statistical computing and graphics. It is popular among data analysts and statisticians due to its powerful statistical analysis capabilities and visualization tools.
+# 
+# SQL: Structured Query Language (SQL) is a domain-specific language used for managing and manipulating relational databases. It is essential for working with large datasets and is often used in conjunction with other programming languages for data analysis.
+# 
+# Java: Java is a general-purpose programming language widely used in enterprise applications, including data science. It is particularly useful for building large-scale, distributed systems and has several libraries and frameworks for machine learning and data analysis, such as Hadoop and Spark.
+
+# Data Scientists commonly utilize a variety of libraries, including:
+# NumPy: NumPy is a library for Python that provides support for large, multi-dimensional arrays and matrices. It includes a wide range of mathematical functions for array operations, making it a fundamental tool for data analysis and scientific computing.
+# 
+# Pandas: Pandas is another Python library that provides high-performance data manipulation and analysis tools. It is particularly useful for working with structured data, such as tabular data, and includes functions for data cleaning, merging, filtering, and more.
+# 
+# Matplotlib: Matplotlib is a Python library for creating static, animated, and interactive visualizations. It provides a variety of plot types, including line, scatter, bar, and histogram plots, and can be used to create publication-quality figures for data analysis and presentation purposes.
+
+# |Data Science Tools|
+# |----|
+# |Jupyter Notebook|
+# |Visual Studio Code|
+# |RStudio|
+
+# <h3>Below are a few examples of evaluating arithmetic expressions in Python.</h3>
+
+# This a simple arithmetic expression to mutiply then add integers
+
+# In[2]:
+
+
+(3*4)+5
+
+
+# this will convert 200 minutes to hours by diving by 60
+
+# In[4]:
+
+
+hours = 200 / 60
+print(hours)
+
+
+# <h1>Objectives</h1>
+
+# •Python (most popular)
+# 
+# •R (industry standard for statistics and data analysis)
+# 
+# •SQL (essential for querying and manipulating data)
+# 
+# •Scala (runs on JVM, used for large-scale data processing)
+# 
+# •java
+# 
+
+# <h2>author</h2>
+
+# Ali fawzi
+
+# In[ ]:
+
+
+
+
